59d3c5c794953bba261c49081d84080cce252d14
Nathan Esquenazi <nesquena@gmail.com>
2014-03-04 22:01:47 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Crouton Sample Code
+
+Displays the use of the [Crouton](https://github.com/keyboardsurfer/Crouton) library with three use cases:
+
+ * Basic preset crouton
+ * Styled crouton
+ * Custom view crouton
+ 
+Screens:
+
+<img src="http://i.imgur.com/4aRRnAe.gif" width="350" />
+<img src="http://i.imgur.com/QlbGZe8.png" width="350" />
+<img src="http://i.imgur.com/LtPAkoX.png" width="350" />
+
+See [this tutorial](http://www.grokkingandroid.com/useful-android-libraries-crouton/) for more details.
---
+++ src/com/codepath/example/croutondemo/CroutonMessageActivity.java
@@ -0,0 +1,49 @@
+package com.codepath.example.croutondemo;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import de.keyboardsurfer.android.widget.crouton.Configuration;
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
+
+public class CroutonMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crouton_message);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.crouton_message, menu);
+		return true;
+	}
+
+	public void onSimpleNotice(View v) {
+		Crouton.showText(this, R.string.simple_text_message, Style.INFO);
+	}
+
+	public void onStyledNotice(View v) {
+	    Configuration croutonConfiguration = new Configuration.Builder().setDuration(2500).build();
+		Style style = new Style.Builder()
+				.setBackgroundColorValue(Color.parseColor("#daffc0"))
+				.setGravity(Gravity.CENTER_HORIZONTAL)
+				.setConfiguration(croutonConfiguration)
+				.setHeight(150)
+				.setTextColorValue(Color.parseColor("#323a2c")).build();
+		Crouton.makeText(this, getString(R.string.styled_text_message), style).show();
+	}
+
+	public void onCustomNotice(View v) {
+		View customView = getLayoutInflater().inflate(R.layout.custom_crouton_layout, null);
+		Crouton.show(this, customView);
+	}
+
+}
